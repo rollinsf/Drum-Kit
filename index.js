efb69d0578b17016873c0f5c51549e91d828c8ee
Joel Rollins <rollinsf97@gmail.com>
2020-02-20 16:12:48 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var button = this.innerHTML;
+        checkKey(button);
+
+        buttonAnimation(button);
+    })
+}
+
+
+document.addEventListener("keydown", function(e) {
+    checkKey(e.key);
+
+    buttonAnimation(e.key)
+});
+
+
+function checkKey(e) {
+    switch (e) {
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default:
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.toggle("pressed");
+
+    setTimeout(function() {activeButton.classList.toggle("pressed");}, 100);
+}
